4a90a02c56db63f41bc101fe44610d580afcd710
phungphuc <phungthienphuc18@gmail.com>
2018-07-20 08:11:27 +0700

    update code

--- src/components/Main/Shop/Shop.js
+++ src/components/Main/Shop/Shop.js
@@ -18,7 +18,9 @@ export default class Shop extends Component {
     }
 
     componentDidMount() {
-        fetch('http://192.168.0.101:27017/gamexc/')
+
+        console.log('go here')
+        fetch('http://10.82.139.8:27017/gamexc/')
         initData()
             .then((responseJson) => {
                 const { category, drawer, item, steam } = responseJson;
